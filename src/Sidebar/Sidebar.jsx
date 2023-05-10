403621653ff809fe823b9c19c43d2af932368906
Denys <denys@Denyss-MacBook-Pro.local>
2023-05-10 13:00:01 +0300

    Sidebar and Workspace is ready

--- src/Sidebar/Sidebar.jsx
+++ src/Sidebar/Sidebar.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import style from './Sidebar.module.css'
 
 const Sidebar = () => {
   return (
-    <div>
-
+    <div className={style.body}>
+      <div className={style.body_inner}>
+        <div className={style.note}>
+          <b className={style.title}>Wow, what a cool note. Wow.</b>
+          <div className={style.info}>
+            <p className={style.date}>12:17 PM</p>
+            <p className={style.text}>This is an amazing note. Can you belive how great this note is? </p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/Workspace/Workspace.jsx
+++ src/Workspace/Workspace.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import style from "./Workspace.module.css";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 
 const Workspace = () => {
+  const [input, setInput] = useState(``);
   return (
-    <div>
-
+    <div className={style.body}>
+      <div className={style.body_inner}>
+        <textarea
+          className={style.textarea}
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+        />
+        {/* <ReactMarkdown children={input} className={style.markdown} /> */}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Workspace
+export default Workspace;
